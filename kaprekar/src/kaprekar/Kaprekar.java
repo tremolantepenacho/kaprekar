87d581f8b4a97c946676ff62a538c98d7151d129
hector <hector@ordenador.arnau.com>
2015-12-19 00:04:42 +0100

    Hecho sin cadenas

--- kaprekar/src/kaprekar/Kaprekar.java
+++ kaprekar/src/kaprekar/Kaprekar.java
@@ -27,11 +27,10 @@ public class Kaprekar {
            InputStreamReader isr = new InputStreamReader(System.in);
            BufferedReader br = new BufferedReader (isr);
            String texto = br.readLine();
-        //   System.out.println("Entrada leida "+texto);
            int cantidad=Integer.parseInt(texto);
           
            for (int i=0;i<cantidad;i++){
-               System.out.println(obtieneKaprekar(br.readLine()));    
+               System.out.println(obtieneKaprekar(Integer.parseInt(br.readLine())));
                
             }
            }
@@ -40,20 +39,21 @@ public class Kaprekar {
         catch(IOException io) {
             System.out.println("No hay caracteres disponibles");
         }
-    }   
+    }  
         
+    
        
-    public static int obtieneKaprekar(String num){
+    public static int obtieneKaprekar(int num){
        
         
-        if (Integer.parseInt(num)%1111==0) return 8;
-        if (Integer.parseInt(num)==6174) return 0;
+        if (num%1111==0) return 8;
+        if (num==6174) return 0;
         int ascDesc[];
-        int diferencia=Integer.parseInt(num);
+        int diferencia=num;
         //int diferencia;
         int i=0;
         do{
-            ascDesc=obtieneAscDesc(Integer.toString(diferencia));
+            ascDesc=obtieneAscDesc(diferencia);
             diferencia=ascDesc[0]-ascDesc[1];
             i++;
         }
@@ -63,33 +63,42 @@ public class Kaprekar {
         return i;
     }
     
-    private static int[] obtieneAscDesc(String num){
+    private static int[] obtieneAscDesc(int num){
         int res[]=new int[4];
-        switch (num.length()) {
-            case 1:  res[0]=Integer.parseInt(num)*1000;
-                     res[1]=Integer.parseInt(num);
-                     return res;
-            case 2:  int aux[]=ordenaDos(num);
-                     res[0]=aux[0]*1000+aux[1]*100;
-                     res[1]=aux[0]+aux[1]*10;
-                     return res;
-            case 3:  int temp[]=ordenaTres(num);
-                     res[0]=temp[0]*1000+temp[1]*100+temp[2]*10;
-                     res[1]=temp[0]+temp[1]*10+temp[2]*100;
-                     return res;
-            case 4:  int var[]=ordenaCuatro(num);
-                     res[0]=var[0]*1000+var[1]*100+var[2]*10+var[3];
-                     res[1]=var[0]+var[1]*10+var[2]*100+var[3]*1000;
-                     return res;
+        int aux[];
+      
+        if (num<10){
+            res[0]=num*1000;
+            res[1]=num;
+            return res;
+            
         }
-        return res;
-         
+        
+        if (num<100){
+            aux=ordenaDos(num);
+            res[0]=aux[0]*1000+aux[1]*100;
+            res[1]=aux[0]+aux[1]*10;
+            return res;
+        }
+        
+        if (num<1000){
+            aux=ordenaTres(num);
+            res[0]=aux[0]*1000+aux[1]*100+aux[2]*10;
+            res[1]=aux[0]+aux[1]*10+aux[2]*100;
+            return res;
+        }
+        else {
+            aux=ordenaCuatro(num);
+            res[0]=aux[0]*1000+aux[1]*100+aux[2]*10+aux[3];
+            res[1]=aux[0]+aux[1]*10+aux[2]*100+aux[3]*1000;
+            return res;
+        }
     }
-    private static int[] ordenaDos(String num){
+    private static int[] ordenaDos(int num){
         
         int res[]=new int[2];
-        int a=num.charAt(0)-48;
-        int b=num.charAt(1)-48;
+        int b=num%10;
+        int a=(num-b)/10;
         if (a>b){
             res[0]=a;
             res[1]=b;
@@ -101,12 +110,14 @@ public class Kaprekar {
         
         return res;
     }
-    private static int[] ordenaTres(String num){
+    private static int[] ordenaTres(int num){
         
         int res[]=new int[3];
-        int a=num.charAt(0)-48;
-        int b=num.charAt(1)-48;
-        int c=num.charAt(2)-48;
+        int c=num%10;
+        num=(num-c)/10;
+        int b=num%10;
+        num=(num-b)/10;
+        int a=num%10;
         
         if(a==b && b==c) {
             res[0]=a;
@@ -156,14 +167,17 @@ public class Kaprekar {
         return res;
     }
     
-    private static int[] ordenaCuatro(String num){
+    private static int[] ordenaCuatro(int num){
               
         int res[]=new int[4];
-        int a=num.charAt(0)-48;
-        int b=num.charAt(1)-48;
-        int c=num.charAt(2)-48;
-        int d=num.charAt(3)-48;
-        
+        int d=num%10;
+        num=(num-d)/10;
+        int c=num%10;
+        num=(num-c)/10;
+        int b=num%10;
+        num=(num-b)/10;
+        int a=num%10;
+                
         if(a==b && b==c && c==d) {
             res[0]=a;
             res[1]=b;
